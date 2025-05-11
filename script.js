a790299319c8790208314734dae09e1386d4d3e9
brenda_j_silva <brenda_j_silva@estudante.sesisenai.org.br>
2025-05-11 20:14:45 -0300

    versão final do site

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ if (menuButton && navMenu) {
 
 // Fecha o menu ao clicar fora
 document.addEventListener('click', (e) => {
-    if (navMenu && navMenu.classList.contains('active') &&
+    if (
+        navMenu &&
+        navMenu.classList.contains('active') &&
         e.target instanceof HTMLElement &&
         !e.target.closest('.nav-menu') &&
-        !e.target.closest('.menu-mobile')) {
+        !e.target.closest('.menu-mobile')
+    ) {
         navMenu.classList.remove('active');
     }
 });
